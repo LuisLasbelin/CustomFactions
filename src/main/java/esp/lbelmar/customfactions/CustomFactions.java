8bc525d4ccd75573e67796086ff7c68adae845e3
Luis Belloch <luis.bellochmartinez@gmail.com>
2021-06-15 10:46:50 +0200

    Initial

---
+++ src/main/java/esp/lbelmar/customfactions/CustomFactions.java
@@ -0,0 +1,17 @@
+package esp.lbelmar.customfactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomFactions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
